d92cd7c9e51b46cd16bc36cd67409ed3d4a1df4d
Micael Grilo <micaelf@outlook.com>
2024-10-10 16:04:11 +0100

    test error

--- project/tests/test_dag.py
+++ project/tests/test_dag.py
@@ -16,6 +16,8 @@ class TestDagIntegrity(unittest.TestCase):
             f"DAG import failures. Errors: {self.dagbag.import_errors}",
         )
     
+    def test_alert(self):
+        self.assertFalse(True, "Alert test failed")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDagIntegrity)
 unittest.TextTestRunner(verbosity=2).run(suite)
