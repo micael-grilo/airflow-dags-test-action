b19e7d6803be8db5dadb3b188efb7774978f1f53
Micael Grilo <micaelf@outlook.com>
2024-10-10 16:34:07 +0100

    Remove test error

--- project/tests/test_dag.py
+++ project/tests/test_dag.py
@@ -15,9 +15,6 @@ class TestDagIntegrity(unittest.TestCase):
             len(self.dagbag.import_errors),
             f"DAG import failures. Errors: {self.dagbag.import_errors}",
         )
-    
-    def test_alert(self):
-        self.assertFalse(True, "Alert test failed")
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDagIntegrity)
 unittest.TextTestRunner(verbosity=2).run(suite)
