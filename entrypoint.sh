91358e9090407cf9a032ee73a38b665e12c9ecfa
Micael Grilo <micaelf@outlook.com>
2024-10-10 15:55:49 +0100

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,9 +29,8 @@ pytest "/github/workspace/$3" -s -q >> result.log
 
 pytest_exit_code=`echo Pytest exited $?`
 echo $pytest_exit_code
-echo "Variable 4: $4"
 
-if [ "$4" = "True" ]; then
+if [ "$4" != "False" ]; then
     echo "Sending output to PR"
     python comment.py --log_filename=result.log --repo_token=$5 
 fi
